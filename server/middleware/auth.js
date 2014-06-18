3426d24d51148314b9409b5b85b69a12576e5f92
Jacob Gable <jacob.gable@gmail.com>
2014-06-17 22:36:16 -0500

    Don't use sessions for token authentication

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,19 +2,17 @@
 var passport = require('passport');
 
 module.exports = {
-	ensureAuthenticated: function (ref) {
-		ref = ref || 'home';
-
+	ensureAuthenticated: function () {
 		return function (req, res, next) {
 			// If this is a request from a logged in user
 			if (req.isAuthenticated()) {
 				return next();
 			} else if (req.get('x-token') || req.query.token) {
 				// If this is an API request with a token
-				return passport.authenticate('token')(req, res, next);
+				return passport.authenticate('token', { session: false })(req, res, next);
 			}
 
-			res.redirect('/login?ref=' + ref);
+			res.send(401, 'Unauthorized');
 		};
 	}
 };
